4df200315f33ddbc06ea2780f0d0d1ae36a33f7b
RavanH <ravanhagen@gmail.com>
2024-07-08 21:21:13 +0200

    standards

--- button/block.asset.php
+++ button/block.asset.php
@@ -1,1 +1,13 @@
-<?php return array( 'dependencies' => array('wp-blocks'),'version' => '0.1');
+<?php
+/**
+ * Orange Confort+ block assets.
+ *
+ * @package Orange Confort+
+ *
+ * @since 0.7
+ */
+
+return array(
+	'dependencies' => array( 'wp-blocks' ),
+	'version'      => '0.1',
+);
